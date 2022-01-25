1c50fed24f4ed9912dedb34c686f018f8020b535
ermzkie <ermawindelynlagustan@gmail.com>
2022-01-25 11:28:53 +0800

    separated file for mock data

--- src/App.test.js
+++ src/App.test.js
@@ -1,101 +1,40 @@
-import {rest} from "msw";
-import {setupServer} from "msw/node";
 import { fireEvent, render, screen } from '@testing-library/react';
 import NewsFeedItem from './components/NewsFeedItem';
 import Sections from './components/Sections';
 import News from './News';
 import App from './App';
 import {getNewsArticles} from './utils/api';
+import {mockData} from "./utils/mockData.js"
 
-/* jest.mock("./utils/api", () => {
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+/* jest.mock("../utils/api", () => {
   return {
-    getNewsArticles: jest.fn().mockImplementation(() => {
-      return {"status":"OK",
-              "copyright":"Copyright (c) 2022 The New York Times Company.  All Rights Reserved.",
-              "num_results":20,
-              "results":[{"uri":"nyt://article/2ff562c3-5739-585d-9ad3-faef214d742d",
-                          "url":"https://www.nytimes.com/article/covid-masks.html",
-                          "id":100000008157965,
-                          "asset_id":100000008157965,
-                          "source":"New York Times",
-                          "published_date":"2022-01-14",
-                          "updated":"2022-01-17 15:22:32",
-                          "section":"Well",
-                          "subsection":"Live",
-                          "nytdsection":"well",
-                          "adx_keywords":"Masks;Content Type: Service;Coronavirus (2019-nCoV);Coronavirus Omicron Variant;Shopping and Retail;Counterfeit Merchandise",
-                          "column":null,
-                          "byline":"By Tara Parker-Pope",
-                          "type":"Article",
-                          "title":"How to Find a Quality Mask (and Avoid Counterfeits)",
-                          "abstract":"Knowing which mask to pick and making sure it’s not a fake requires the sleuthing skills of a forensic investigator. Our guide can help.",
-                          "des_facet":["Masks","Content Type: Service","Coronavirus (2019-nCoV)","Coronavirus Omicron Variant","Shopping and Retail","Counterfeit Merchandise"],
-                          "org_facet":[],
-                          "per_facet":[],
-                          "geo_facet":[],
-                          "media":[{"type":"image",
-                                    "subtype":"photo",
-                                    "caption":"",
-                                    "copyright":"Charlie Rubin for The New York Times",
-                                    "approved_for_syndication":1,
-                                    "media-metadata":[{"url":"https://static01.nyt.com/images/2022/02/13/well/13well-n95mask-guide1/13well-n95mask-guide1-thumbStandard-v2.jpg",
-                                                        "format":"Standard Thumbnail","height":75,"width":75},
-                                                      {"url":"https://static01.nyt.com/images/2022/02/13/well/13well-n95mask-guide1/13well-n95mask-guide1-mediumThreeByTwo210.jpg",
-                                                        "format":"mediumThreeByTwo210","height":140,"width":210},
-                                                      {"url":"https://static01.nyt.com/images/2022/02/13/well/13well-n95mask-guide1/13well-n95mask-guide1-mediumThreeByTwo440.jpg",
-                                                        "format":"mediumThreeByTwo440","height":293,"width":440}]}
-                                  ],
-                          "eta_id":0}]};
+      getNewsArticles: jest.fn().mockImplementation(() => {
+      return mockData;
     }),
   };
 }); */
 
-/* test("renders learn react", async () => {
-    const {findByText} = render(<App/>);
-    const element = await findByText("All");
-    expect(element).toBeInTheDocument();
-}) */
-
 test("API testing",async () =>{
-  fetch.mockResponseOnce(JSON.stringify({"status":"OK",
-  "copyright":"Copyright (c) 2022 The New York Times Company.  All Rights Reserved.",
-  "num_results":20,
-  "results":[{"uri":"nyt://article/2ff562c3-5739-585d-9ad3-faef214d742d",
-              "url":"https://www.nytimes.com/article/covid-masks.html",
-              "id":100000008157965,
-              "asset_id":100000008157965,
-              "source":"New York Times",
-              "published_date":"2022-01-14",
-              "updated":"2022-01-17 15:22:32",
-              "section":"Well",
-              "subsection":"Live",
-              "nytdsection":"well",
-              "adx_keywords":"Masks;Content Type: Service;Coronavirus (2019-nCoV);Coronavirus Omicron Variant;Shopping and Retail;Counterfeit Merchandise",
-              "column":null,
-              "byline":"By Tara Parker-Pope",
-              "type":"Article",
-              "title":"How to Find a Quality Mask (and Avoid Counterfeits)",
-              "abstract":"Knowing which mask to pick and making sure it’s not a fake requires the sleuthing skills of a forensic investigator. Our guide can help.",
-              "des_facet":["Masks","Content Type: Service","Coronavirus (2019-nCoV)","Coronavirus Omicron Variant","Shopping and Retail","Counterfeit Merchandise"],
-              "org_facet":[],
-              "per_facet":[],
-              "geo_facet":[],
-              "media":[{"type":"image",
-                        "subtype":"photo",
-                        "caption":"",
-                        "copyright":"Charlie Rubin for The New York Times",
-                        "approved_for_syndication":1,
-                        "media-metadata":[{"url":"https://static01.nyt.com/images/2022/02/13/well/13well-n95mask-guide1/13well-n95mask-guide1-thumbStandard-v2.jpg",
-                                            "format":"Standard Thumbnail","height":75,"width":75},
-                                          {"url":"https://static01.nyt.com/images/2022/02/13/well/13well-n95mask-guide1/13well-n95mask-guide1-mediumThreeByTwo210.jpg",
-                                            "format":"mediumThreeByTwo210","height":140,"width":210},
-                                          {"url":"https://static01.nyt.com/images/2022/02/13/well/13well-n95mask-guide1/13well-n95mask-guide1-mediumThreeByTwo440.jpg",
-                                            "format":"mediumThreeByTwo440","height":293,"width":440}]}
-                      ],
-              "eta_id":0}]}));
+  fetch.mockResponseOnce(JSON.stringify(mockData));
   var data = await getNewsArticles();
   expect(data.num_results).toEqual(20);
 });
+
+/* 
+test("API testing",async () =>{
+  var data = await getNewsArticles();
+  expect(data.num_results).toEqual(20);
+}); */
+
+/*  test("renders an a tag for `All`", async () => {
+  const {findByText} = render(<App/>);
+  const element = await findByText("All");
+  expect(element).toBeInTheDocument();    
+}) */
 
 const media = [{type: "image",
                   subtype: "photo",
--- src/utils/api.test.js
+++ src/utils/api.test.js
@@ -1,13 +1,12 @@
 import { getNewsArticles } from "./api";
+import {mockData} from "./mockData.js";
 
 beforeEach(() => {
     fetch.resetMocks();
 });
 
 it("convers correctly", async () => {
-    fetch.mockResponseOnce(JSON.stringify({num_results: 20}));
-
+    fetch.mockResponseOnce(JSON.stringify(mockData));
     const response = await getNewsArticles();
-
     console.log(response);
 })
